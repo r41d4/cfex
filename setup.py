b7f1d766f3133900cb7f3614013565cda1163112
Arthur <cybcomnet@gmail.com>
2022-10-19 21:24:47 +0300

    Add Python package configuration files
    - Add pyproject.toml
    - Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cfex",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click",
+    ],
+    entry_points={"console_scripts": ["cfex=cfex"]},
+)
